b5bfbbdbb2eadb47d1b78d836fc6aeaccaea3023
naruun labs <taekwondori@gmail.com>
2026-09-02 11:28:46 +0900

    화살표 방향 스위치 · 청홍 소리 구분

--- sw.js
+++ sw.js
@@ -17,7 +17,7 @@
 //    그러면 예전 캐시가 통째로 비워집니다.
 // =========================================================
 
-const CACHE_NAME = 'naruunlabs-2026-09-02-autonext';
+const CACHE_NAME = 'naruunlabs-2026-09-02b-arrowsw';
 
 const APP_SHELL = [
   './',
