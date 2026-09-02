5baf9ffab2e0fe8ded1065deca0a888a1b939882
naruun labs <taekwondori@gmail.com>
2026-09-02 11:53:25 +0900

    서버 판정값 적용 경로 수정

--- sw.js
+++ sw.js
@@ -17,7 +17,7 @@
 //    그러면 예전 캐시가 통째로 비워집니다.
 // =========================================================
 
-const CACHE_NAME = 'naruunlabs-2026-09-02c-count';
+const CACHE_NAME = 'naruunlabs-2026-09-02d-tunefix';
 
 const APP_SHELL = [
   './',
