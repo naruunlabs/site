5192023562a4d8df5a2d87f1d709356369963497
naruun labs <taekwondori@gmail.com>
2026-09-11 13:38:49 +0900

    aijumpmaster 온라인연결

--- sw.js
+++ sw.js
@@ -17,7 +17,7 @@
 //    그러면 예전 캐시가 통째로 비워집니다.
 // =========================================================
 
-const CACHE_NAME = 'naruunlabs-2026-09-02g-main-short-copy';
+const CACHE_NAME = 'naruunlabs-2026-09-11a-jumpmaster-v3-1';
 
 const APP_SHELL = [
   './',
