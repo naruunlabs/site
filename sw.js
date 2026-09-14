21caf4ccb28492cbbb377bfc369ae255cd75f1fd
naruun labs <taekwondori@gmail.com>
2026-09-14 22:36:00 +0900

    단체 리듬 발자국 간격과 도착 발판 개선

--- sw.js
+++ sw.js
@@ -17,7 +17,7 @@
 //    그러면 예전 캐시가 통째로 비워집니다.
 // =========================================================
 
-const CACHE_NAME = 'naruunlabs-2026-09-14-jump-v331';
+const CACHE_NAME = 'naruunlabs-2026-09-14-jump-v332';
 
 const APP_SHELL = [
   './',
