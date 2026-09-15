29d65a2c9d892329d476c7e0ad2e9391779dbb73
naruun labs <taekwondori@gmail.com>
2026-09-15 15:01:32 +0900

    점프마스터 단체 리듬 음악 추가

--- sw.js
+++ sw.js
@@ -17,7 +17,7 @@
 //    그러면 예전 캐시가 통째로 비워집니다.
 // =========================================================
 
-const CACHE_NAME = 'naruunlabs-2026-09-15-kick322-jump340';
+const CACHE_NAME = 'naruunlabs-2026-09-15-jump-group-music';
 
 const APP_SHELL = [
   './',
