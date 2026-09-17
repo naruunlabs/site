7e5092600fea7caf3a9ce414d31b2d3b659f3a0c
naruun labs <taekwondori@gmail.com>
2026-09-17 06:13:07 +0000

    STRIKE 아이콘 각진 글자꼴로

--- sw.js
+++ sw.js
@@ -17,7 +17,7 @@
 //    그러면 예전 캐시가 통째로 비워집니다.
 // =========================================================
 
-const CACHE_NAME = 'naruunlabs-2026-09-16-jump-infer-scale';
+const CACHE_NAME = 'naruunlabs-2026-09-17-strike-icon';
 
 const APP_SHELL = [
   './',
