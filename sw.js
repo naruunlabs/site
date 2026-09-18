932245a5d99a762d45dd173d8aba339aedab076f
naruun labs <taekwondori@gmail.com>
2026-09-18 05:48:42 +0000

    사이트 갱신

--- sw.js
+++ sw.js
@@ -17,7 +17,7 @@
 //    그러면 예전 캐시가 통째로 비워집니다.
 // =========================================================
 
-const CACHE_NAME = 'naruunlabs-2026-09-18-strike-v090';
+const CACHE_NAME = 'naruunlabs-2026-09-18-strike-purple';
 
 const APP_SHELL = [
   './',
